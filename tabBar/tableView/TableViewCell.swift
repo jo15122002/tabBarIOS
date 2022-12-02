61e0291a1367b17c756e29376513e42e4971068c
digital <digital@iMac24-8.local>
2022-12-02 09:01:53 +0100

    ajout de la table view

---
+++ tabBar/tableView/TableViewCell.swift
@@ -0,0 +1,57 @@
+import Foundation
+//
+//  FirstTableViewCell.swift
+//  tableView
+//
+//  Created by digital on 29/11/2022.
+//
+
+import UIKit
+
+protocol HeaderViewDelegate{
+    func didStartEditing()
+}
+
+class HeaderView: UIView, UITextFieldDelegate {
+
+    @IBOutlet weak var searchField: UITextField!
+    var delegate:HeaderViewDelegate?
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder){
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        searchField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        print("Begin edition")
+        delegate?.didStartEditing()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("return pressed")
+        return true
+    }
+    
+    static func makeInstanceFromNib() -> HeaderView
+    {
+        return UINib(nibName: "HeaderView", bundle: .main).instantiate(withOwner: nil, options: nil).first as! HeaderView
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if let curString = textField.text{
+            print(curString + string)
+        }
+        return true
+    }
+
+}
---
+++ tabBar/tableView/TableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by digital on 29/11/2022.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, HeaderViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var data = ["riri", "fifi", "loulou"]
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var header = HeaderView.makeInstanceFromNib()
+        header.delegate = self
+        
+        header.backgroundColor = UIColor.red
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell = tableView.dequeueReusableCell(withIdentifier: "second", for: indexPath)
+        
+        cell.textLabel?.text = self.data[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 100
+        }
+        
+        if indexPath.row == 1 {
+            return 200
+        }
+        
+        if indexPath.row == 2 {
+            return 300
+        }
+        
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(indexPath.row == 0){
+            for i in 0..<data.count{
+                data[i] = "J'ai faim"
+            }
+            tableView.reloadData()
+        }else{
+            print(data[indexPath.row])
+        }
+    }
+    
+    func didStartEditing() {
+        print("In did start editing from view controller")
+        data = []
+        tableView.reloadData()
+        //data = data.popFirst()
+    }
+}
+
